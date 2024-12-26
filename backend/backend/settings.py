283e73b90ac5e694d98391dc31777bcf56e39de2
hayhuhin <hayhuhin.new@gmail.com>
2024-12-26 12:50:33 +0200

    Docker added for BE and FE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,11 +26,11 @@ REST_FRAMEWORK = {
     },
 }
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
